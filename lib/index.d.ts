b4dcc8a07ead34acb35af7d0c5a909a41f17e99b
Subhash Halder <halder.subhash@gmail.com>
2020-04-19 22:16:10 +0530

    Added support for whole crop box dragging and cursor indicator

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,12 @@
 interface Oprions {
     id: string;
     imageURL: string;
-    cropImageRLT?: (dataUrl: string) => void;
+    cropImageRLT: (imageURL: string) => void;
+    cropMaskColor?: string;
+    cropCornerColor?: string;
+    cropCornerLineWidth?: number;
+    dragCornerBoxSize?: number;
 }
-declare const _default: (options: Oprions) => () => string;
+declare const _default: (options: Oprions) => CanvasRenderingContext2D;
 export default _default;
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 interface Oprions {
   id: string;
   imageURL: string;
-  cropImageRLT?: (dataUrl: string) => void;
+  cropImageRLT: (imageURL: string) => void;
+  cropMaskColor?: string;
+  cropCornerColor?: string;
+  cropCornerLineWidth?: number;
+  dragCornerBoxSize?: number;
 }
 
-export default (options: Oprions) => {
+export default (options: Oprions): CanvasRenderingContext2D => {
+  const dragCornerBoxSize = options.dragCornerBoxSize || 20;
+  const cropImageRLT = options.cropImageRLT;
+  const cropMaskColor = options.cropMaskColor || 'rgba(0, 0, 0, 0.5)';
+  const cropCornerColor = options.cropCornerColor || 'green';
+  const cropCornerLineWidth = options.cropCornerLineWidth || 5;
+
   const imageDiv = document.getElementById(options.id);
   if (!imageDiv) {
     throw Error('Please provide a valid html element Id to work with');
@@ -27,23 +37,60 @@ export default (options: Oprions) => {
   );
   const exportCanvas = document.createElement('canvas');
   exportCanvas.setAttribute('style', 'display:none');
+
+  // div for click tracker
+  const topLeftCornerDiv = document.createElement('div');
+  const topRightCornerDiv = document.createElement('div');
+  const bottomRightCornerDiv = document.createElement('div');
+  const bottomLeftCornerDiv = document.createElement('div');
+  const selectionDiv = document.createElement('div');
+  bottomLeftCornerDiv.setAttribute(
+    'style',
+    `position: absolute; width: ${dragCornerBoxSize}px; height: ${dragCornerBoxSize}px; top: 0px; left: 0px; cursor: sw-resize; background: transparent; stroke: transparent;`
+  );
+  bottomRightCornerDiv.setAttribute(
+    'style',
+    `position: absolute; width: ${dragCornerBoxSize}px; height: ${dragCornerBoxSize}px; top: 0px; left: 0px; cursor: se-resize; background: transparent; stroke: transparent;`
+  );
+  topRightCornerDiv.setAttribute(
+    'style',
+    `position: absolute; width: ${dragCornerBoxSize}px; height: ${dragCornerBoxSize}px; top: 0px; left: 0px; cursor: ne-resize; background: transparent; stroke: transparent;`
+  );
+  topLeftCornerDiv.setAttribute(
+    'style',
+    `position: absolute; width: ${dragCornerBoxSize}px; height: ${dragCornerBoxSize}px; top: 0px; left: 0px; cursor: nw-resize; background: transparent; stroke: transparent;`
+  );
+  selectionDiv.setAttribute(
+    'style',
+    `position: absolute; width: ${dragCornerBoxSize}px; height: ${dragCornerBoxSize}px; top: 0px; left: 0px; cursor: all-scroll; background: transparent; stroke: transparent;`
+  );
+
+  containerDiv.appendChild(exportCanvas);
   containerDiv.appendChild(imageCanvas);
   containerDiv.appendChild(cropCanvas);
-  containerDiv.appendChild(exportCanvas);
-  const imageCtx = imageCanvas.getContext('2d');
-  if (!imageCtx) {
+  containerDiv.appendChild(selectionDiv);
+  containerDiv.appendChild(bottomLeftCornerDiv);
+  containerDiv.appendChild(bottomRightCornerDiv);
+  containerDiv.appendChild(topRightCornerDiv);
+  containerDiv.appendChild(topLeftCornerDiv);
+  const imageCanvasCtx = imageCanvas.getContext('2d');
+  if (!imageCanvasCtx) {
     throw Error('Not able to create canvas');
   }
   const cropCanvasCtx = cropCanvas.getContext('2d');
   if (!cropCanvasCtx) {
     throw Error('Not able to create canvas');
   }
-  let getCropImage: () => string = () => '';
+  const exportCanvasCtx = exportCanvas.getContext('2d');
+  if (!exportCanvasCtx) {
+    throw Error('Not able to create canvas');
+  }
+  let drawExportCanvas: () => void;
   let dragTopLeft = false;
   let dragBottomLeft = false;
   let dragTopRight = false;
   let dragBottomRight = false;
-  const dragBoxSize = 20;
+  let dragBox = false;
   const image = new Image();
   image.src = options.imageURL;
   image.onload = () => {
@@ -51,10 +98,54 @@ export default (options: Oprions) => {
     imageCanvas.height = image.height;
     cropCanvas.width = imageCanvas.clientWidth;
     cropCanvas.height = imageCanvas.clientHeight;
-    imageCtx.drawImage(image, 0, 0);
-    cropCanvas.addEventListener('mousedown', mouseDown, false);
     cropCanvas.addEventListener('mouseup', mouseUp, false);
     cropCanvas.addEventListener('mousemove', mouseMove, false);
+    imageCanvasCtx.drawImage(image, 0, 0);
+    bottomLeftCornerDiv.addEventListener(
+      'mousedown',
+      () => {
+        dragBottomLeft = true;
+      },
+      false
+    );
+    bottomLeftCornerDiv.addEventListener('mouseup', mouseUp, false);
+    bottomLeftCornerDiv.addEventListener('mousemove', mouseMove, false);
+    topLeftCornerDiv.addEventListener(
+      'mousedown',
+      () => {
+        dragTopLeft = true;
+      },
+      false
+    );
+    topLeftCornerDiv.addEventListener('mouseup', mouseUp, false);
+    topLeftCornerDiv.addEventListener('mousemove', mouseMove, false);
+    topRightCornerDiv.addEventListener(
+      'mousedown',
+      () => {
+        dragTopRight = true;
+      },
+      false
+    );
+    topRightCornerDiv.addEventListener('mouseup', mouseUp, false);
+    topRightCornerDiv.addEventListener('mousemove', mouseMove, false);
+    bottomRightCornerDiv.addEventListener(
+      'mousedown',
+      () => {
+        dragBottomRight = true;
+      },
+      false
+    );
+    bottomRightCornerDiv.addEventListener('mouseup', mouseUp, false);
+    bottomRightCornerDiv.addEventListener('mousemove', mouseMove, false);
+    selectionDiv.addEventListener(
+      'mousedown',
+      () => {
+        dragBox = true;
+      },
+      false
+    );
+    selectionDiv.addEventListener('mouseup', mouseUp, false);
+    selectionDiv.addEventListener('mousemove', mouseMove, false);
 
     const selection = {
       x: 0,
@@ -63,7 +154,7 @@ export default (options: Oprions) => {
       h: imageCanvas.clientHeight,
     };
 
-    getCropImage = () => {
+    drawExportCanvas = () => {
       const scaleX = image.width / cropCanvas.width;
       const scaleY = image.height / cropCanvas.height;
       const x = selection.x * scaleX;
@@ -72,61 +163,33 @@ export default (options: Oprions) => {
       const height = selection.h * scaleY;
       exportCanvas.width = width;
       exportCanvas.height = height;
-      const expCanvasCtx = exportCanvas.getContext('2d');
-      if (!expCanvasCtx) {
-        throw Error('Not able to create canvas');
-      }
-      expCanvasCtx.drawImage(image, x, y, width, height, 0, 0, width, height);
-      return exportCanvas.toDataURL('image/jpeg');
+      exportCanvasCtx.drawImage(
+        image,
+        x,
+        y,
+        width,
+        height,
+        0,
+        0,
+        width,
+        height
+      );
+      cropImageRLT && cropImageRLT(exportCanvas.toDataURL());
     };
 
-    function mouseDown(e: MouseEvent) {
-      if (!imageDiv) {
-        throw Error('Not able to create canvas');
-      }
-      const mouseX = e.pageX - imageDiv.offsetLeft;
-      const mouseY = e.pageY - imageDiv.offsetTop;
-      (window as any).cropCanvas = cropCanvas;
-
-      if (
-        checkDragable(mouseX, selection.x) &&
-        checkDragable(mouseY, selection.y)
-      ) {
-        dragTopLeft = true;
-      } else if (
-        checkDragable(mouseX, selection.x + selection.w) &&
-        checkDragable(mouseY, selection.y)
-      ) {
-        dragTopRight = true;
-      } else if (
-        checkDragable(mouseX, selection.x) &&
-        checkDragable(mouseY, selection.y + selection.h)
-      ) {
-        dragBottomLeft = true;
-      } else if (
-        checkDragable(mouseX, selection.x + selection.w) &&
-        checkDragable(mouseY, selection.y + selection.h)
-      ) {
-        dragBottomRight = true;
-      }
-
-      draw();
-    }
-
-    function checkDragable(p1: number, p2: number) {
-      return Math.abs(p1 - p2) < dragBoxSize;
-    }
-
     function mouseUp() {
-      dragTopLeft = dragTopRight = dragBottomLeft = dragBottomRight = false;
+      dragTopLeft = dragTopRight = dragBottomLeft = dragBottomRight = dragBox = false;
     }
+
+    let mousePrevX = 0;
+    let mousePrevY = 0;
 
     function mouseMove(e: MouseEvent) {
       if (!imageDiv) {
         throw Error('Not able to create canvas');
       }
-      const mouseX = e.pageX - imageDiv.offsetLeft;
-      const mouseY = e.pageY - imageDiv.offsetTop;
+      let mouseX = e.pageX - imageDiv.offsetLeft;
+      let mouseY = e.pageY - imageDiv.offsetTop;
       if (dragTopLeft) {
         selection.w += selection.x - mouseX;
         selection.h += selection.y - mouseY;
@@ -143,7 +206,24 @@ export default (options: Oprions) => {
       } else if (dragBottomRight) {
         selection.w = Math.abs(selection.x - mouseX);
         selection.h = Math.abs(selection.y - mouseY);
+      } else if (dragBox) {
+        selection.x += mouseX - mousePrevX;
+        selection.y += mouseY - mousePrevY;
       }
+      if (selection.x < 0) {
+        selection.x = 0;
+      }
+      if (selection.x + selection.w > cropCanvas.width) {
+        selection.x = cropCanvas.width - selection.w;
+      }
+      if (selection.y < 0) {
+        selection.y = 0;
+      }
+      if (selection.y + selection.h > cropCanvas.height) {
+        selection.y = cropCanvas.height - selection.h;
+      }
+      mousePrevX = mouseX;
+      mousePrevY = mouseY;
       draw();
     }
 
@@ -152,9 +232,9 @@ export default (options: Oprions) => {
         throw Error('Not able to create canvas');
       }
       cropCanvasCtx.clearRect(0, 0, cropCanvas.width, cropCanvas.height);
-      cropCanvasCtx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-      cropCanvasCtx.strokeStyle = '#00ff00';
-      cropCanvasCtx.lineWidth = 5;
+      cropCanvasCtx.fillStyle = cropMaskColor;
+      cropCanvasCtx.strokeStyle = cropCornerColor;
+      cropCanvasCtx.lineWidth = cropCornerLineWidth;
       cropCanvasCtx.fillRect(0, 0, cropCanvas.width, cropCanvas.height);
       cropCanvasCtx.clearRect(
         selection.x,
@@ -163,46 +243,56 @@ export default (options: Oprions) => {
         selection.h
       );
       cropCanvasCtx.beginPath();
-      cropCanvasCtx.moveTo(selection.x, selection.y + dragBoxSize);
+      cropCanvasCtx.moveTo(selection.x, selection.y + dragCornerBoxSize);
       cropCanvasCtx.lineTo(selection.x, selection.y);
-      cropCanvasCtx.lineTo(selection.x + dragBoxSize, selection.y);
+      cropCanvasCtx.lineTo(selection.x + dragCornerBoxSize, selection.y);
       cropCanvasCtx.moveTo(
-        selection.x + selection.w - dragBoxSize,
+        selection.x + selection.w - dragCornerBoxSize,
         selection.y
       );
       cropCanvasCtx.lineTo(selection.x + selection.w, selection.y);
       cropCanvasCtx.lineTo(
         selection.x + selection.w,
-        selection.y + dragBoxSize
+        selection.y + dragCornerBoxSize
       );
       cropCanvasCtx.moveTo(
         selection.x + selection.w,
-        selection.y + selection.h - dragBoxSize
+        selection.y + selection.h - dragCornerBoxSize
       );
       cropCanvasCtx.lineTo(
         selection.x + selection.w,
         selection.y + selection.h
       );
       cropCanvasCtx.lineTo(
-        selection.x + selection.w - dragBoxSize,
+        selection.x + selection.w - dragCornerBoxSize,
         selection.y + selection.h
       );
       cropCanvasCtx.moveTo(
-        selection.x + dragBoxSize,
+        selection.x + dragCornerBoxSize,
         selection.y + selection.h
       );
       cropCanvasCtx.lineTo(selection.x, selection.y + selection.h);
       cropCanvasCtx.lineTo(
         selection.x,
-        selection.y + selection.h - dragBoxSize
+        selection.y + selection.h - dragCornerBoxSize
       );
       cropCanvasCtx.stroke();
-      options.cropImageRLT && options.cropImageRLT(getCropImage());
+      topLeftCornerDiv.style.top = topRightCornerDiv.style.top =
+        selection.y + 'px';
+      topLeftCornerDiv.style.left = bottomLeftCornerDiv.style.left =
+        selection.x + 'px';
+      topRightCornerDiv.style.left = bottomRightCornerDiv.style.left =
+        selection.x + selection.w - dragCornerBoxSize + 'px';
+      bottomLeftCornerDiv.style.top = bottomRightCornerDiv.style.top =
+        selection.y + selection.h - dragCornerBoxSize + 'px';
+      selectionDiv.style.top = selection.y + 'px';
+      selectionDiv.style.left = selection.x + 'px';
+      selectionDiv.style.width = selection.w + 'px';
+      selectionDiv.style.height = selection.h + 'px';
+      drawExportCanvas();
     }
 
     draw();
   };
-  return () => {
-    return getCropImage();
-  };
+  return exportCanvasCtx;
 };
